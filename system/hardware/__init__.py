5c108d43800e6b2e71cf34f3acbc75c30e87cefe
Oxygen Liu <oxygen.liu@gmail.com>
2026-03-11 15:19:13 +0800

    Lazy-import hardware classes to avoid pulling Tici deps on RK3588
    Tici hardware class imports zstandard, GPIO, etc. that aren't available
    on RK3588. Move all hardware class imports into their respective
    conditional branches.

--- system/hardware/__init__.py
+++ system/hardware/__init__.py
@@ -2,8 +2,6 @@ import os
 from typing import cast
 
 from openpilot.system.hardware.base import HardwareBase
-from openpilot.system.hardware.tici.hardware import Tici
-from openpilot.system.hardware.pc.hardware import Pc
 
 TICI = os.path.isfile('/TICI')
 AGNOS = os.path.isfile('/AGNOS')
@@ -20,9 +18,11 @@ PC = not TICI and not RK3588
 
 
 if TICI:
+  from openpilot.system.hardware.tici.hardware import Tici
   HARDWARE = cast(HardwareBase, Tici())
 elif RK3588:
   from openpilot.system.hardware.rk3588.hardware import Rk3588
   HARDWARE = cast(HardwareBase, Rk3588())
 else:
+  from openpilot.system.hardware.pc.hardware import Pc
   HARDWARE = cast(HardwareBase, Pc())
